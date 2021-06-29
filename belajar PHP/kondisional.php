8c3d168a7ac76febaf016808f85a3cbee7cad467
Paldy <paldiengineer@yahoo.com>
2021-06-29 20:58:12 +0700

    tambahkan switch case file

--- belajar PHP/kondisional.php
+++ belajar PHP/kondisional.php
@@ -1,11 +1,18 @@
 <?php 
-
+// Belajar pengkondisian
 
 $derajat = 35;
 
 if( $derajat > 35 ) {
 
- echo ""
+ echo "Alhamdulillah kamu masih sehat";
+
+} elseif ( $derajat == 35 ) {
+
+  echo "Kamu masih aman";
+
+} else {
+  echo "Kamu masih sehat";
 
 }
 
---
+++ belajar PHP/pengkondisianswitch.php
@@ -0,0 +1,35 @@
+<?php 
+
+
+// Pengkondisian denga switch case 
+
+$warna = "Merah";
+
+switch ($warna) {
+
+    case 'Merah';
+      echo "Berhenti";
+    break;
+
+    case 'Kuning';
+      echo "hati hati";
+    break;
+
+    case 'Hijau';
+      echo "Maju ke depan";
+    break;
+
+    default;
+      echo "Lampu sudah rusak";
+    break;
+}
+
+
+
+
+
+
+
+
+
+?>
