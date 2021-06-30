e527e9d4f561c1958fdf15ca7ab9fcbd016c9794
Paldy <paldiengineer@yahoo.com>
2021-06-30 22:43:04 +0700

    menambahkan for & foreach php

---
+++ belajar PHP/for.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+
+for ($i=0; $i < 10; $i++) { 
+  echo "Saya bernilai" . $i . '</br>';
+}
+
+// Jika $i = 0 maka akan dijalankan perulangannya sebanyak < 10 ,dan karena $i++ maka akan ditambah 1
+
+
+
+?>
