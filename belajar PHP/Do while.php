6942a0e26b48d4850d5e14f321b0a5ed0197c1f0
Paldy <paldiengineer@yahoo.com>
2021-06-30 16:38:43 +0700

    menambahkan do & while

---
+++ belajar PHP/Do while.php
@@ -0,0 +1,14 @@
+<?php 
+
+
+$b = 20;
+
+do {
+  echo "Saya bernilai " . $a . "</br>"; 
+} while ($b <= 10);
+
+// Sama seperti menggunakan while bedanya kalo do while ini ngecek dulu baru di jalankan
+
+
+
+?>
---
+++ belajar PHP/while.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+$a = 12;
+
+while ($a <= 15 ) {
+
+   echo "Saya bernilai" . $a . '</br>';
+   $a++;
+
+}
+
+
+
+
+
+
+
+?>
