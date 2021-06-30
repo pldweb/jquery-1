2a79f1a339c1cfa1e7e2081ae19b8ea3c0057261
Paldy <paldiengineer@yahoo.com>
2021-06-30 22:49:41 +0700

    menambahkan foreach php

--- belajar PHP/foreach.php
+++ belajar PHP/foreach.php
@@ -1,8 +1,15 @@
 <?php 
 
+$buah = ['tomat','pepaya','pisang'];
 
 
+foreach ($buah as $key => $value) {
+  echo $value . '</br>';
+}
 
+// panggil variable buah dan key menjadi value 
+// lalu tampilkan $value yaitu tomat pepaya pisang
+// Jika echo $key maka yang ditampilkan adalah 0 1 2 3 / angka dari value itu sendiri 
 
 
 
